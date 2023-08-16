dae88b028c28de97fcdc40d339ecc7752bbb861b
P-D-E-E <2890897553@qq.com>
2023-08-16 18:01:59 +0800

    lab2 done.

---
+++ lab2/IntList/ALittleTry.java
@@ -0,0 +1,9 @@
+package IntList;
+
+public class ALittleTry {
+    public static void main(String[] args){
+        IntList lst = IntList.of(1, 2, 3);
+
+        System.out.println(lst.toString());
+    }
+}
