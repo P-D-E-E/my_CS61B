f28c30e81878125e9ceed74ee9e5fb32df7239bb
P-D-E-E <2890897553@qq.com>
2023-08-24 11:39:37 +0800

    modify Filk.java

---
+++ course_code/Lecture16/BST.java
@@ -0,0 +1,18 @@
+public class BinarySearchTree{
+
+    private int item;
+    public BinarySearchTree(int i){
+        item = i
+    }
+
+    public void Insert(BinarySearchTree T, Item i){
+        if (T == null) {
+            return new BinarySearchTree<Item>(i);
+        }
+        if (i.compareTo(T.root.item) < 0){
+
+        }
+    }
+
+
+}
